5f918bbb67269ba14bb896f819534e97ddad2be1
Víctor Bettachini <victorb@gmx.net>
2023-09-09 19:20:36 -0300

    ia

---
+++ meine/exp/HT3440/sort.r
@@ -0,0 +1,13 @@
+"get BO_log.txt into a data.frame
+sort by 
+save to file
+"
+
+library("data.table")
+getwd()
+bo_log_dt <- fread("./exp/HT3440/BO_log.txt", header = TRUE, sep = "\t", stringsAsFactors = FALSE)
+class(bo_log_dt)
+setorder(bo_log_dt, -ganancia)
+
+# write to file
+fwrite(bo_log_dt, "./exp/HT3440/BO_log_sorted.txt", sep = "\t", row.names = FALSE)
